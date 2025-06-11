fbabda5a4ad11d91c87995d758e39f38cb8d48c2
oluiscabral <oluiscabral@hotmail.com>
2025-06-11 19:52:18 -0300

    v0.6.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 33
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = System.getenv("RELEASE_VERSION") ?: "dev"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
